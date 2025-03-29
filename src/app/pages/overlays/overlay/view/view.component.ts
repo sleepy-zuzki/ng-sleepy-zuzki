a8ac95d042f998a04d0cfe3d1d7f862cc23fe216
Zuzky <sleepyzuzki@gmail.com>
2025-03-29 18:10:23 -0500

    remove api token from project

--- src/app/pages/overlays/overlay/view/view.component.ts
+++ src/app/pages/overlays/overlay/view/view.component.ts
@@ -1,12 +1,10 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, effect, ElementRef, OnInit, Signal, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HeaderComponent } from '@components/header/header.component';
 import { ApiService } from '@services/api.service';
-import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-view',
-  imports: [HeaderComponent],
+  imports: [],
   templateUrl: './view.component.html',
   styleUrl: './view.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
@@ -37,12 +35,7 @@ export class ViewComponent implements OnInit {
     const streamer: string | null = this.route.snapshot.paramMap.get('streamer');
 
     if (streamer !== null) {
-      const httpParams: HttpParams = new HttpParams()
-      .set('filters[overlay][slug]', streamer)
-      .set('populate[thumbnail][fields][0]', 'formats')
-      .set('populate[content][fields][0]', 'url');
-
-      this.apiService.fetchOverlayViews(httpParams)
+      this.apiService.fetchOverlayViews()
     }
   }
 }
--- src/app/pages/overlays/overlays.component.ts
+++ src/app/pages/overlays/overlays.component.ts
@@ -1,15 +1,12 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, Signal } from '@angular/core';
-import { HeaderComponent } from '@components/header/header.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ApiService } from '@services/api.service';
 import { Datum } from '@core/Interfaces/StrapiResponse';
 import { SkeletonComponent } from '@components/projects/project/skeleton/skeleton.component';
-import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-overlays',
   imports: [
-    HeaderComponent,
     RouterLink,
     RouterLinkActive,
     SkeletonComponent
@@ -20,21 +17,14 @@ import { HttpParams } from '@angular/common/http';
 })
 export class OverlaysComponent implements OnInit {
 
-  overlays: Signal<Datum[]>;
+  overlays: Signal<any[]>;
 
   constructor(private apiService: ApiService) {
     this.overlays = this.apiService.overlays;
   }
 
   ngOnInit(): void {
-    const httpParams: HttpParams = new HttpParams()
-    .set('populate[screenshot][fields][0]', 'formats')
-    .set('populate[creators][fields]', '*')
-    .set('fields[0]', 'id')
-    .set('fields[1]', 'streamer')
-    .set('fields[2]', 'slug');
-
-    this.apiService.fetchOverlays(httpParams);
+    this.apiService.fetchOverlays();
   }
 
 }
