5a716ee28d48b4c8f28bfa2878e5ed9113125860
Zuzky <sleepyzuzki@gmail.com>
2025-03-29 18:11:00 -0500

    update project structure to reduce the number of files and change api provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, input, InputSignal, ViewChild } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
+  @ViewChild('drawer') drawer?: ElementRef;
   title = 'Sleepy Zuzki';
+  readonly views: InputSignal<any[]> = input<any[]>([]);
+
+  openDrawer(): void {
+    if (this.drawer) {
+      this.drawer.nativeElement.open = true;
+    }
+  }
 }
--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,40 +1,38 @@
 import { computed, Injectable, Signal, signal, WritableSignal } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Datum, StrapiResponse } from '@core/Interfaces/StrapiResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  #projects: WritableSignal<Datum[]> = signal([]);
-  projects: Signal<Datum[]> = computed(this.#projects);
+  #projects: WritableSignal<[]> = signal([]);
+  projects: Signal<[]> = computed(this.#projects);
 
-  #overlays: WritableSignal<Datum[]> = signal([]);
-  overlays: Signal<Datum[]> = computed(this.#overlays);
+  #overlays: WritableSignal<[]> = signal([]);
+  overlays: Signal<[]> = computed(this.#overlays);
 
-  #views: WritableSignal<Datum[]> = signal([]);
+  #views: WritableSignal<[]> = signal([]);
   views: Signal<any[]> = computed(this.#views);
 
   constructor(private http: HttpClient) { }
 
   fetchProjects(params?: HttpParams): void {
     if (this.projects().length === 0) {
-      this.http.get<StrapiResponse>(`api/projects`, {params})
-      .subscribe((res: StrapiResponse): void => this.#projects.set(res.data));
+      this.http.get<any>(`projects`, {params})
+      .subscribe((res: any): void => this.#projects.set(res.data));
     }
   }
 
   fetchOverlays(params?: HttpParams): void {
-    this.http.get<StrapiResponse>(`api/overlays`, {params})
-    .subscribe((res: StrapiResponse): void => this.#overlays.set(res.data));
+    this.http.get<any>(`overlays`, {params})
+    .subscribe((res: any): void => this.#overlays.set(res));
   }
 
   fetchOverlayViews(params?: HttpParams): void {
-    this.http.get<StrapiResponse>(`api/views`, {params})
-    .subscribe((res: StrapiResponse): void => {
-      console.log(res);
-      this.#views.set(res.data ?? [])
+    this.http.get<any>(`views`, {params})
+    .subscribe((res: any): void => {
+      this.#views.set(res)
     });
   }
 }
