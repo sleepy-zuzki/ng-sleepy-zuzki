900ac80dd70c5012d11ba3085b7b63f9cec4f0ee
Zuzky <sleepy@zuzki.dev>
2025-05-01 08:31:39 -0500

    Update styles, semantic structures, and enhance UI components
    Reorganized the layout and class structures in the home page, improved semantic naming, and integrated FontAwesome icons in the footer. Adjusted UI elements for better clarity.
    
    ---
    
    # [Home.Feature]
    Overall code improvements for structure, naming, and styles in the home page layout.
    - Updated image styling and applied utility class changes for consistency.
    - Corrected section IDs to use proper kebab-case for semantics and improved readability.
    - Converted text and forms for better accessibility.
    - Wrapped the contact form inside an HTML `<form>` element for proper structure.
    - Improved text spacing and design aesthetics in sections like "About Me" and "Technologies".
    
    # [Footer.Component]
    Enhanced the footer by integrating icons and importing necessary dependencies.
    - Integrated FontAwesome brand icons for GitHub, Twitch, and Twitter links.
    - Replaced text labels with icons for a modernized design.
    - Imported required modules (FontAwesome) and defined icon constants within the footer component file.
    
    # [Header.Component]
    Commented out unnecessary header buttons to simplify the UI.
    - Temporarily disabled buttons for language switch and mobile menu toggle in the header.
    - Intended to streamline the user experience by removing redundant or inactive features.
    
    Signed-off-by: Zuzky <sleepy@zuzki.dev>

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LinkComponent } from '@components/ui/link/link.component';
+import { faGithub, faTwitch, faXTwitter } from '@awesome.me/kit-15d5a6a4b5/icons/classic/brands';
+import { FaIconComponent, IconDefinition } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterModule, LinkComponent],
+  imports: [CommonModule, RouterModule, LinkComponent, FaIconComponent],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
+
+  readonly faGithub: IconDefinition = faGithub;
+  readonly faXTwitter: IconDefinition = faXTwitter;
+  readonly faTwitch: IconDefinition = faTwitch;
+
 }
