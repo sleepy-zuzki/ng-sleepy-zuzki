2610e1f92d92a6feefe71955bb502f0e88cc071b
Zuzki <sleepy@zuzki.dev>
2025-05-11 12:25:22 -0500

    Refactor Work Details Feature to Work Details Component
    This update replaces the deprecated Work Details Feature with a newly defined Work Details Component. The changes enhance structure, maintainability, and functionality.
    
    ---
    
    ### [WorkDetailsFeature]
    **General:** Removed the old feature structure.
    
    - Deleted `WorkDetailsFeature` class and its associated files.
    - Removed outdated HTML, CSS, and TS implementations.
    
    ### [WorkDetailsComponent]
    **General:** Introduced a new component-based architecture for work details.
    
    - Created a `WorkDetailsComponent` with improved routing and functionality.
    - Implemented dynamic overlay and layouts management logic.
    - Added `openLayoutsMenu` and `changeLayout` methods with effects-based updates.
    - Defined services and signals for data binding and interaction.
    
    ### [WorkDetailsComponent.Html]
    **General:** Added modern HTML structure and implementation.
    
    - Designed a layout switcher using dialog components.
    - Integrated button and iframe elements for user interaction and display.
    
    ### [WorkDetailsComponent.Css]
    **General:** Applied Tailwind for better styling and transition animations.
    
    - Defined styles for dialog elements such as layout menus.
    - Added animations for menu appearance and transitions.
    
    ### [WorkDetails.Page]
    **General:** Updated imports to use the new component structure.
    
    - Replaced `WorkDetailsFeature` with `WorkDetailsComponent`.
    - Adjusted imports and schema compatibility.
    
    Signed-off-by: Zuzki <sleepy@zuzki.dev>

--- src/app/pages/work-details.page.ts
+++ src/app/pages/work-details.page.ts
@@ -1,11 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { WorkDetailsFeature } from '@features/works/details/work-details.feature';
+import { WorkDetailsComponent } from '@features/works/details/work-details.component';
 
 @Component({
   selector: 'app-work-details',
   standalone: true,
   imports: [
-    WorkDetailsFeature
+    WorkDetailsComponent
   ],
   template: `<app-work-detail-feature />`,
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
